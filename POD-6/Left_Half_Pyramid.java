a291e5dad1df5b94da0d8b22eaa3d12579738ce4
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-11-11 10:31:57 +0000

    java

---
+++ POD-6/Left_Half_Pyramid.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Left_Half_Pyramid {
+    public static void Print_Left_Half_Pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            //print spaces 
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            //print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        Print_Left_Half_Pyramid(size);
+    }
+}
